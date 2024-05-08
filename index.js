a6397e96ce865568f769aafd780ca07438ffe0ef
Josue Marquez <owaruuu@gmail.com>
2024-05-08 12:27:56 -0400

    Update index.js
    -auto formatting
    -change 'id' to 'lectureId'

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-let button = document.getElementById('button');
-button.addEventListener('click', ProcessFile);
+let button = document.getElementById("button");
+button.addEventListener("click", ProcessFile);
 
-let buttonKanji = document.getElementById('button-kanji');
-buttonKanji.addEventListener('click', ProcessFileKanji);
+let buttonKanji = document.getElementById("button-kanji");
+buttonKanji.addEventListener("click", ProcessFileKanji);
 
-function ProcessFile(){
-    let idValue = document.getElementById('id').value;
-    let nameValue = document.getElementById('name').value;
-    let file = document.getElementById('file').files[0];
+function ProcessFile() {
+    let idValue = document.getElementById("id").value;
+    let nameValue = document.getElementById("name").value;
+    let file = document.getElementById("file").files[0];
 
-    file.text()
-    .then(text => {ParseFile(idValue, nameValue, text);})
+    file.text().then((text) => {
+        ParseFile(idValue, nameValue, text);
+    });
 }
 
-function ParseFile(idValue, nameValue, text){
+function ParseFile(idValue, nameValue, text) {
     console.log("Parsing file");
     let obj = {};
 
-    obj["id"] = idValue;
+    obj["lectureId"] = idValue;
     obj["name"] = nameValue;
     obj["termList"] = [];
 
@@ -33,55 +34,55 @@ function ParseFile(idValue, nameValue, text){
         let char = text.charAt(i);
 
         //busco por un tab, necesito buscar 2 tab por linea
-        if (char === '\t') {
+        if (char === "\t") {
             //encontre el primer tab
-            if(secondTab == false){
-                tempTerm = text.substr(start, i - start)
+            if (secondTab == false) {
+                tempTerm = text.substr(start, i - start);
                 start = i;
                 secondTab = true;
-            }else{
-            //encontre el segundo tab 
-                tempExtra = text.substr(start+1, i - start - 1)
+            } else {
+                //encontre el segundo tab
+                tempExtra = text.substr(start + 1, i - start - 1);
                 start = i;
-            }          
+            }
         }
 
         //aqui encuentro una nueva linea y guardo lo que llevo
         //dentro del objeto nuevo de termino y luego lo agrego
         //al 'lecture' array
-        if (char === '\n') {
-            tempAnswer = text.substr(start+1, i - start - 2);//el 2 es necesario para remover '/r/n' del texto
+        if (char === "\n") {
+            tempAnswer = text.substr(start + 1, i - start - 2); //el 2 es necesario para remover '/r/n' del texto
             let tempObj = {
-                "id" : id,
-                "term" : tempTerm,
-                "extra" : tempExtra,
-                "answer" : tempAnswer,
+                id: id,
+                term: tempTerm,
+                extra: tempExtra,
+                answer: tempAnswer,
             };
             obj["termList"].push(tempObj);
-            start = i+1;
+            start = i + 1;
             id++;
             secondTab = false;
             tempExtra = "";
         }
-        
     }
-    
+
     console.log(obj);
     console.log(JSON.stringify(obj));
 
     //retornar este objeto
 }
 
-function ProcessFileKanji(){
-    let idValue = document.getElementById('id-kanji').value;
-    let nameValue = document.getElementById('name-kanji').value;
-    let file = document.getElementById('file-kanji').files[0];
+function ProcessFileKanji() {
+    let idValue = document.getElementById("id-kanji").value;
+    let nameValue = document.getElementById("name-kanji").value;
+    let file = document.getElementById("file-kanji").files[0];
 
-    file.text()
-    .then(text => {ParseFileKanji(idValue, nameValue, text);})
+    file.text().then((text) => {
+        ParseFileKanji(idValue, nameValue, text);
+    });
 }
 
-function ParseFileKanji(idValue, nameValue, text){
+function ParseFileKanji(idValue, nameValue, text) {
     console.log("Parsing file");
     let obj = {};
 
@@ -95,39 +96,39 @@ function ParseFileKanji(idValue, nameValue, text){
     let id = 0;
     let start = 0;
 
-    let checkbox = document.getElementById('checkbox-kanji');
+    let checkbox = document.getElementById("checkbox-kanji");
 
     //por cada letra del texto
     for (let i = 0; i < text.length; i++) {
         let char = text.charAt(i);
 
         //busco tabs
-        if (char === '\t') {
+        if (char === "\t") {
             //encontre el primer tab, guarde la respuesta
-            if(secondTab == false){
-                tempAnswer = text.substr(start, i - start)
+            if (secondTab == false) {
+                tempAnswer = text.substr(start, i - start);
                 start = i;
                 secondTab = true;
-            }else{
-            //encontre el segundo tab, guardo kanji 
-                tempTerm = text.substr(start+1, i - start - 1)
+            } else {
+                //encontre el segundo tab, guardo kanji
+                tempTerm = text.substr(start + 1, i - start - 1);
                 start = i;
-            }          
+            }
         }
 
         //aqui encuentro una nueva linea, si tengo algo en tempTerm
         //lo guardo en el 'lecture' array
-        if (char === '\n') {
-            if(checkbox.checked){
-                tempAnswer += " / " + text.substr(start+1, i - start - 2);
+        if (char === "\n") {
+            if (checkbox.checked) {
+                tempAnswer += " / " + text.substr(start + 1, i - start - 2);
             }
 
-            if(tempTerm !== ""){
+            if (tempTerm !== "") {
                 let tempObj = {
-                    "id" : id,
-                    "term" : tempTerm,
-                    "extra" : "",
-                    "answer" : tempAnswer,
+                    id: id,
+                    term: tempTerm,
+                    extra: "",
+                    answer: tempAnswer,
                 };
 
                 obj["termList"].push(tempObj);
@@ -136,11 +137,11 @@ function ParseFileKanji(idValue, nameValue, text){
             }
             // tempAnswer = text.substr(start+1, i - start - 2);//el 2 es necesario para remover '/r/n' del texto
 
-            start = i+1;          
+            start = i + 1;
             secondTab = false;
-        }   
+        }
     }
-    
+
     console.log(obj);
     console.log(JSON.stringify(obj));
 }
